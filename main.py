4ca9df744846e3f1f9919486000556bd16b123bf
Jobair2020 <jobairnahian2017@gmail.com>
2024-06-28 11:26:57 +0600

    basket ball with library function added

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import math
 import numpy as np
 
 # Initialize the Video
-capture = cv2.VideoCapture('Videos/vid (6).mp4')
+capture = cv2.VideoCapture('Videos/vid (1).mp4')
 hsvVals = {'hmin': 0, 'smin': 115, 'vmin': 0, 'hmax': 15, 'smax': 255, 'vmax': 255}
 
 # Variables
@@ -43,20 +43,20 @@ while True:
         Y = [pos[1] for pos in positionList]
         A, B, C = np.polyfit(X, Y, 2)
 
-        # for i, pos in enumerate(positionList):
-        #     # todo
-        #     cv2.circle(imgContours, pos, 8, (0, 255, 0), cv2.FILLED)
-        #     if i != 0:
-        #         # todo
-        #         cv2.line(imgContours, pos, positionList[i - 1], (0, 255, 0), 5)
+        for i, pos in enumerate(positionList):
+            # todo
+            cv2.circle(imgContours, pos, 8, (0, 255, 0), cv2.FILLED)
+            if i != 0:
+                # todo
+                cv2.line(imgContours, pos, positionList[i - 1], (0, 255, 0), 5)
 
-        # for x in xList:
-        #     y = int(A * x * x + B * x + C)
-        #     cv2.circle(imgContours, (x, y), 2, (255, 0, 255), cv2.FILLED)
+        for x in xList:
+            y = int(A * x * x + B * x + C)
+            cv2.circle(imgContours, (x, y), 2, (255, 0, 255), cv2.FILLED)
 
         if len(positionList) < 10:
             # Prediction
-            # x values 325 to 425 and y value 590
+            # x values 325 to 425 and y value 590 ax^2+bx+c = 0
             a = A
             b = B
             c = C - 590
@@ -73,8 +73,11 @@ while True:
     # Display
     # todo
     imgContours = cv2.resize(imgContours, (0, 0), None, 0.7, 0.6)
-    # cv2.imshow("image", img)
-    cv2.imshow("imageColor", imgContours)
+    mask2 = cv2.resize(mask, (0, 0), None, 0.7, 0.6)
+    cv2.imshow("mask", mask2)
+    cv2.imshow("imageContors", imgContours)
+    # cv2.imshow("ballDetection",imgColor)
+
     cv2.waitKey(100)
 
 # if __name__ == '__main__':
